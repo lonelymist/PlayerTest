181b3f0c920e890055de4f200d5d905d5bf78373
boolenboom <32370777+boolenboom@users.noreply.github.com>
2019-07-26 17:49:52 +0800

    V8.1.1_攝影機碰撞

---
+++ Test/Assets/Script/CameraMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject CameraOffset;
+    public GameObject CameraZoom;
+    private RaycastHit CamColliderDetect;
+    [SerializeField]
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.DrawRay(CameraZoom.transform.position, this.transform.position - CameraZoom.transform.position * 20);
+        if(Physics.Raycast(CameraZoom.transform.position, this.transform.position - CameraZoom.transform.position, out CamColliderDetect, 20))
+        {
+
+            float distanceOffect = CamColliderDetect.distance / Vector3.Distance(CameraZoom.transform.position, CameraOffset.transform.position);
+            Debug.Log(distanceOffect);
+            if (distanceOffect < 1)
+            {
+
+                this.transform.position = Vector3.Lerp(this.transform.position,CameraZoom.transform.position,distanceOffect)/*Vector3.Lerp(OriLocalPosition, CloestPosition, 1 - distanceOffect)*/;
+            }
+        }
+        else
+        {
+            this.transform.position = CameraOffset.transform.position;
+        }
+    }
+}
